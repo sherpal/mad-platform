cb687976488a9d91cc002fdb71438671b1c434c4
Antoine Doeraene <antoine.doeraene@communardo.com>
2026-09-17 12:08:41 +0200

    Further optimize NatArray based on Claude's benchmark

--- game/src/main/scalajvm/be/doeraene/perf/NatArray.scala
+++ game/src/main/scalajvm/be/doeraene/perf/NatArray.scala
@@ -22,16 +22,16 @@ object NatArray {
   extension [T](arr: NatArray[T]) {
     private[perf] inline def toArray: Array[T] = arr
 
-    def toVector: Vector[T] = Vector.from(arr)
+    inline def toVector: Vector[T] = Vector.from(arr)
+    inline def toList: List[T]     = List.from(arr)
 
     inline def map[U](f: T => U)(using ClassTag[U]): NatArray[U] = arr.mapOps(f)
 
     inline def flatMap[U](f: T => NatArray[U])(using ClassTag[U]): NatArray[U] = arr.flatMapOps(f)
 
-    inline def foreach(f: T => Unit): Unit = {
-      arr.map(f)
-      ()
-    }
+    inline def flatMapOpt[U](f: T => Option[U])(using ClassTag[U]): NatArray[U] = arr.flatMapItOps(f)
+
+    inline def foreach(f: T => Unit): Unit = arr.foreachOps(f)
 
     inline def filter(predicate: T => Boolean): NatArray[T]     = arr.filterOps(predicate)
     inline def withFilter(predicate: T => Boolean): NatArray[T] = arr.filter(predicate)
@@ -85,7 +85,5 @@ object NatArray {
 
   given [T](using Encoder[T]): Encoder[NatArray[T]]              = summon[Encoder[Vector[T]]].contramap(_.toVector)
   given [T](using Decoder[T], ClassTag[T]): Decoder[NatArray[T]] = summon[Decoder[Vector[T]]].map(Converters.toNatArray)
-
-  given [T](using ClassTag[T]): Conversion[Option[T], NatArray[T]] = _.fold(empty)(apply(_))
 
 }
